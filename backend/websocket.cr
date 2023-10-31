265e45e58b0f6831d2af607309410fb6d65cc8d0
Rachel Gonzalez Rodriguez <61662217+Rachel-21@users.noreply.github.com>
2023-10-31 11:59:18 -0400

    websocket testing

---
+++ backend/websocket.cr
@@ -0,0 +1,47 @@
+require "http/web_socket"
+
+PORT = 9000
+
+class Client
+    property socket: TCPSocket
+
+    def initialize(@socket)
+    end
+
+    def send(message, cmd)
+        socket.send ({message: message, cmd: cmd}).json
+    end 
+end
+
+Class Server 
+    property Users = Array(Client).new
+
+    def handle_connection(socket)
+        pp"New connection: #{socket}"
+
+        temp_client = Client.new(socket)
+        client << temp_client
+
+        loop do
+            new_message = socket.gets
+
+            clients.each do |obj|
+                obj.send new_message, "CHAT"
+            end
+        end
+
+    rescue e
+        clients.delete temp_client
+        pp "#{temp_client} has disconnected"
+
+    def initialize
+        pp "server is listening to new connections on #{PORT}..."
+
+        server = TCPServer.new(PORT)
+
+        while socket = server.accept?
+            spawn handle_connection(socket)
+        end
+    end
+end
+
